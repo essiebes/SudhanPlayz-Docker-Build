6a6fae91d54bbcc4125ff7c8e30b7637bed7f95a
essiebes <67075950+essiebes@users.noreply.github.com>
2022-06-20 16:25:28 +0200

    Return yes no instead of exit codes

--- scripts/checkver.sh
+++ scripts/checkver.sh
@@ -2,7 +2,7 @@
 currentver="$1"
 requiredver="4.0.0"
  if [ "$(printf '%s\n' "$requiredver" "$currentver" | sort -V | head -n1)" = "$requiredver" ]; then 
-        exit 0
+        echo "yes"
  else
-        exit 1
+        echo "no"
  fi
